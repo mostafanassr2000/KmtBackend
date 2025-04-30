0ae08aab89a22ba0b3834b9604eedd72c1e24359
Ahmed Samir <a.anwar@GenOr.com>
2025-04-30 15:59:47 +0300

    .

--- KmtBackend.Infrastructure/TokenGenerator/JwtTokenGenerator.cs
+++ KmtBackend.Infrastructure/TokenGenerator/JwtTokenGenerator.cs
@@ -1,7 +1,6 @@
 using KmtBackend.DAL.Entities;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
-using System.Collections;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
